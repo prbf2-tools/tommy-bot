9c71f4608be46d64813987999f33c71c0e74eb3f
cassius23 <emilekm@gmail.com>
2023-04-03 11:22:16 +0200

    Refactor log handling

---
+++ functions/logs/ban.js
@@ -0,0 +1,56 @@
+const BAN_PUB_CH = '995387208947204257';
+const BAN_ADMIN_CH = '995520998554218557';
+
+export function handleBan(dataBans, client) {
+    var banLogSplit = dataBans.split(' ')
+    console.log(banLogSplit)
+    var banAdmin = banLogSplit[banLogSplit.length - 2]
+    var banName = banLogSplit[3] + ' ' + banLogSplit[4]
+    var banHash = banLogSplit[2]
+    var banIP = banLogSplit[5]
+    if (banLogSplit[banLogSplit.length - 1] === '(perm)\r') {
+        var banDuration = 'Permanently'
+    } else if (banLogSplit[banLogSplit.length - 1] === '(round)\r') {
+        var banDuration = 'Current Round'
+    } else {
+        var banTimeRaw = banLogSplit[banLogSplit.length - 1].replace('(', '').replace(')', '')
+        var banTimeHour = Number(banTimeRaw) / 3600
+        var banDuration = banTimeHour + ' Hours'
+    }
+    var banReason = banLogSplit.splice(6, banLogSplit.length).join(' ').split('banned by')[0]
+
+    const banSendPub = {
+        color: 0x991b0d,
+        title: 'Banned player: ' + banName,
+        description: '\n**Reason:** ' + banReason
+            + '\n**Duration:** ' + banDuration,
+        footer: {
+            text: banAdmin + ' In-Game'
+        },
+        timestamp: new Date(),
+    }
+    var geoready = 'INVALID'
+    var banIP = banLogSplit[5];
+    var geo2 = geoip.lookup(banIP);
+    if (geo2 === null) {
+        var geoready = "white";
+    } else {
+        var geoready = geo2.country.toLowerCase();
+    }
+    var banIPSafe = banIP.split('.')
+
+    const banSendAdmin = {
+        color: 0x991b0d,
+        title: 'BANNED',
+        description: '**Name:** `' + banName
+            + '`\n**By:** `' + banAdmin
+            + '`\n**Reason:** ' + banReason
+            + '\n**Duration:** ' + banDuration
+            + '\n**Hash-ID:** `' + banHash
+            + '`\n**IP:** `' + banIPSafe[0] + '.' + banIPSafe[1] + '.***.***` :flag_' + geoready + ':',
+        timestamp: new Date(),
+    }
+
+    client.channels.cache.get(BAN_PUB_CH).send({ embeds: [banSendPub] }); //742795954729517077
+    client.channels.cache.get(BAN_ADMIN_CH).send({ embeds: [banSendAdmin] });
+}
---
+++ functions/logs/join.js
@@ -0,0 +1,40 @@
+const geoip = require('geoip-lite');
+
+const LOG_JOIN_CH = '995521059119960144';
+
+export function handleJoin(dataJoin, client) {
+    let joinLoglines = dataJoin.trim().split("\n")
+    var joinLogFormat = joinLoglines[joinLoglines.length - 1].toString('utf-8')
+    var joinLogSplit = joinLogFormat.split('\t')
+    if (joinLogSplit[6] == '(LEGACY)') {
+        var accType = 'Legacy'
+    } else if (joinLogSplit[6] == '(VAC BANNED)') {
+        var accType = 'VAC Banned'
+    } else if (joinLogSplit[6] == '(LEGACY)(VAC BANNED)') {
+        var accType = 'Legacy and VAC Banned'
+    } else {
+        var accType = 'Standard'
+    }
+    var geoready = 'INVALID'
+    var ip = joinLogSplit[5];
+    var geo = geoip.lookup(ip);
+    if (geo === null) {
+        var geoready = "white";
+    } else {
+        var geoready = geo.country.toLowerCase();
+    }
+
+
+
+    const joinLogPost = {
+        color: 0X6500B3,
+        title: joinLogSplit[3],
+        description: '**Hash-ID: **`' + joinLogSplit[1]
+            + '`\n**IP: **`' + joinLogSplit[5] + '` :flag_' + geoready
+            + ':\n**Account Level: **' + joinLogSplit[2]
+            + '\n**Account Type: **' + accType
+            + '\n**Creation Date: **' + joinLogSplit[4]
+            + '\n**Joined: **' + joinLogSplit[0].replace('[', '').replace(']', ''),
+    }
+    client.channels.cache.get(LOG_JOIN_CH).send({ embeds: [joinLogPost] });
+}
