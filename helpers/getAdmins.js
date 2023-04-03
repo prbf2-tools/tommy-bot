f93ff9b625098c9eb6866c0cf1481ca6c8bc2965
unknown <moekhalil70@gmail.com>
2023-04-03 23:53:30 +0300

    Updated Admin Rights - 2

--- helpers/getAdmins.js
+++ helpers/getAdmins.js
@@ -10,35 +10,35 @@ const rolesObj = {
   "1062472543275057212": "trial", //Trial Admin
 };
 
-const getAdmins = (client) => {
+const getAdmins = async (client) => {
   const roles = Object.keys(rolesObj);
-  setInterval(async () => {
-    try {
-      const guild = await client.guilds.fetch(process.env.GUILD_ID);
-      const members = await guild.members.fetch();
-      const ownerId = guild.ownerId;
-      const hashDb = db.get("hashDb");
-      await guild.members.fetch(ownerId);
-      for (const member of members) {
-        const memberId = member[1].id;
-        const memberRoles = member[1]._roles;
-        const commonRoles = getCommonValues(memberRoles, roles);
-        if (commonRoles.length === 0) continue;
-        const adminRolesString = commonRoles.map((role) => rolesObj[role]);
-        const role = adminRolesString.includes("senior")
-          ? "senior"
-          : adminRolesString.includes("admin")
-          ? "admin"
-          : "trial";
-        const hashRecord = hashDb.find({ id: memberId }).value();
-        if (!hashRecord || hashRecord.role === role) continue;
-        hashDb.find({ id: memberId }).assign({ role }).write();
-      }
-      insertAdmins();
-    } catch (e) {
-      console.log("Error getting admins", e);
+  // setInterval(async () => {
+  try {
+    const guild = await client.guilds.fetch(process.env.GUILD_ID);
+    const members = await guild.members.fetch();
+    const ownerId = guild.ownerId;
+    const hashDb = db.get("hashDb");
+    await guild.members.fetch(ownerId);
+    for (const member of members) {
+      const memberId = member[1].id;
+      const memberRoles = member[1]._roles;
+      const commonRoles = getCommonValues(memberRoles, roles);
+      if (commonRoles.length === 0) continue;
+      const adminRolesString = commonRoles.map((role) => rolesObj[role]);
+      const role = adminRolesString.includes("senior")
+        ? "senior"
+        : adminRolesString.includes("admin")
+        ? "admin"
+        : "trial";
+      const hashRecord = hashDb.find({ id: memberId }).value();
+      if (!hashRecord || hashRecord.role === role) continue;
+      hashDb.find({ id: memberId }).assign({ role }).write();
     }
-  }, 1000 * 60); // 1 minutes
+    insertAdmins();
+  } catch (e) {
+    console.log("Error getting admins", e);
+  }
+  // }, 1000 * 60); // 1 minutes
 };
 
 module.exports = { getAdmins };
