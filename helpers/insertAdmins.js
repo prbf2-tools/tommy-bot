cff28acb9d064b876d83301c5e2685258c763b6e
unknown <moekhalil70@gmail.com>
2023-04-03 23:23:46 +0300

    Auto Insert Admins - Test Phase 2

--- helpers/insertAdmins.js
+++ helpers/insertAdmins.js
@@ -3,52 +3,56 @@ const { db } = require("../db/db.js");
 const path = require("path");
 
 const insertAdmins = () => {
-  const pathToRealityAdmin =
-    "C:UsersAdministratorDesktopMafiamodsprpythongame\realityconfig_admin.py";
-  fs.copyFileSync(
-    pathToRealityAdmin,
-    path.join(pathToDb, "realityconfig_admin.py")
-  );
-  const pathToDb = path.join(__dirname, "db");
-  const realityconfigFile = fs.readFileSync(
-    path.join(pathToDb, "realityconfig_admin.py"),
-    "utf8"
-  );
-  const hashDb = db.get("hashDb").value();
-  const filterRoleless = hashDb.filter((hash) => !hash.role);
-  const objectToReplace = realityconfigFile.match(
-    /adm_adminHashes = {[\s\S]*?}/
-  );
-  let str = "{\n";
-  for (let i = 0; i < filterRoleless.length; i++) {
-    const adminHash = filterRoleless[i].hashId;
-    const adminHashNoSpace = adminHash.replace(/\s/g, "");
-    const adminName = filterRoleless[i].ingameName;
-    const adminRole = filterRoleless[i].role;
-    const adminRoleNumber =
-      adminRole === "senior"
-        ? "0"
-        : adminRole === "admin"
-        ? "1"
-        : adminRole === "trial"
-        ? "2"
-        : "3";
-    if (adminRoleNumber === "3") {
-      console.log(`Error with ${adminHashNoSpace}!`);
-      continue;
+  try {
+    const pathToRealityAdmin =
+      "C:/Users/Administrator/Desktop/Mafia/mods/pr/python/game/realityconfig_admin.py";
+    fs.copyFileSync(
+      pathToRealityAdmin,
+      path.join(pathToDb, "realityconfig_admin.py")
+    );
+    const pathToDb = path.join(__dirname, "db");
+    const realityconfigFile = fs.readFileSync(
+      path.join(pathToDb, "realityconfig_admin.py"),
+      "utf8"
+    );
+    const hashDb = db.get("hashDb").value();
+    const filterRoleless = hashDb.filter((hash) => !hash.role);
+    const objectToReplace = realityconfigFile.match(
+      /adm_adminHashes = {[\s\S]*?}/
+    );
+    let str = "{\n";
+    for (let i = 0; i < filterRoleless.length; i++) {
+      const adminHash = filterRoleless[i].hashId;
+      const adminHashNoSpace = adminHash.replace(/\s/g, "");
+      const adminName = filterRoleless[i].ingameName;
+      const adminRole = filterRoleless[i].role;
+      const adminRoleNumber =
+        adminRole === "senior"
+          ? "0"
+          : adminRole === "admin"
+          ? "1"
+          : adminRole === "trial"
+          ? "2"
+          : "3";
+      if (adminRoleNumber === "3") {
+        console.log(`Error with ${adminHashNoSpace}!`);
+        continue;
+      }
+      str += `    "${adminHashNoSpace}":  "${adminRole}",  #${adminName}\n`;
     }
-    str += `    "${adminHashNoSpace}":  "${adminRole}",  #${adminName}\n`;
+    str += "}";
+    const realityconfigFileUpdated = realityconfigFile.replace(
+      objectToReplace,
+      `adm_adminHashes = ${str}`
+    );
+    fs.writeFileSync(
+      path.join(pathToDb, "realityconfig_admin.py"),
+      realityconfigFileUpdated
+    );
+    console.log("Admins inserted!");
+  } catch (err) {
+    console.log(`Error Inserting Admins: ${err}`);
   }
-  str += "}";
-  const realityconfigFileUpdated = realityconfigFile.replace(
-    objectToReplace,
-    `adm_adminHashes = ${str}`
-  );
-  fs.writeFileSync(
-    path.join(pathToDb, "realityconfig_admin.py"),
-    realityconfigFileUpdated
-  );
-  console.log("Admins inserted!");
 };
 
 module.exports = { insertAdmins };
