82a487d18fd6fb6b0855820dde6785a3dfbac41e
cassius23 <emilekm@gmail.com>
2023-04-07 11:56:15 +0200

    Add channels functions

--- db/db.js
+++ db/db.js
@@ -3,6 +3,9 @@ import FileSync from "lowdb/adapters/FileSync.js";
 
 const adapter = new FileSync("./db/db.json");
 const db = low(adapter);
-db.defaults({ hashDb: [] }).write();
+db.defaults({
+  hashDb: [],
+  channels: []
+}).write();
 
 export default db;
