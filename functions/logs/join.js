8f39cdef90d022c156be70681d9d9f1ff806abc2
cassius23 <emilekm@gmail.com>
2023-04-12 15:23:46 +0200

    Use modern JS definitions

--- functions/logs/join.js
+++ functions/logs/join.js
@@ -3,7 +3,7 @@ import Tail from "always-tail";
 import geoip from "geoip-lite";
 
 export const watchJoin = (client) => {
-    var filenameJoin = "logs/joinlog.log";
+    const filenameJoin = "logs/joinlog.log";
     if (!fs.existsSync(filenameJoin)) fs.writeFileSync(filenameJoin, "");
     const tailJoin = new Tail(filenameJoin, "\n");
 
@@ -17,11 +17,12 @@ export const watchJoin = (client) => {
 };
 
 const process = (client) => {
-    return (dataJoin) => {
-        let joinLoglines = dataJoin.trim().split("\n");
-        var joinLogFormat = joinLoglines[joinLoglines.length - 1].toString("utf-8");
-        var joinLogSplit = joinLogFormat.split("\t");
-        var accType = "Standard";
+    return (data) => {
+        const joinLoglines = data.trim().split("\n");
+        const joinLogFormat = joinLoglines[joinLoglines.length - 1].toString("utf-8");
+        const joinLogSplit = joinLogFormat.split("\t");
+
+        let accType = "Standard";
         if (joinLogSplit[6] == "(LEGACY)") {
             accType = "Legacy";
         } else if (joinLogSplit[6] == "(VAC BANNED)") {
@@ -29,24 +30,23 @@ const process = (client) => {
         } else if (joinLogSplit[6] == "(LEGACY)(VAC BANNED)") {
             accType = "Legacy and VAC Banned";
         }
-        var geoready = "INVALID";
-        var ip = joinLogSplit[5];
-        var geo = geoip.lookup(ip);
-        if (geo === null) {
-            geoready = "white";
-        } else {
-            geoready = geo.country.toLowerCase();
+
+        const ip = joinLogSplit[5];
+        let geoready = "white";
+        const geo2 = geoip.lookup(ip);
+        if (geo2 !== null) {
+            geoready = geo2.country.toLowerCase();
         }
 
         const joinLogPost = {
             color: 0X6500B3,
             title: joinLogSplit[3],
             description: "**Hash-ID: **`" + joinLogSplit[1]
-        + "`\n**IP: **`" + joinLogSplit[5] + "` :flag_" + geoready
-        + ":\n**Account Level: **" + joinLogSplit[2]
-        + "\n**Account Type: **" + accType
-        + "\n**Creation Date: **" + joinLogSplit[4]
-        + "\n**Joined: **" + joinLogSplit[0].replace("[", "").replace("]", ""),
+                + "`\n**IP: **`" + joinLogSplit[5] + "` :flag_" + geoready
+                + ":\n**Account Level: **" + joinLogSplit[2]
+                + "\n**Account Type: **" + accType
+                + "\n**Creation Date: **" + joinLogSplit[4]
+                + "\n**Joined: **" + joinLogSplit[0].replace("[", "").replace("]", ""),
         };
         client.channels.cache.get("995521059119960144").send({ embeds: [joinLogPost] });
     };
