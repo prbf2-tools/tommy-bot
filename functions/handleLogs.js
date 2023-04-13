0416b5e7e440cdd5d90f94787de46cc8be13c0c0
cassius23 <emilekm@gmail.com>
2023-04-13 15:17:14 +0200

    Fix imports

--- functions/handleLogs.js
+++ functions/handleLogs.js
@@ -1,7 +1,7 @@
 
-import { watchBanlist } from "./logs/bans";
-import { watchCommands } from "./logs/commands";
-import { watchJoin } from "./logs/join";
+import { watchBanlist } from "./logs/bans.js";
+import { watchCommands } from "./logs/commands.js";
+import { watchJoin } from "./logs/join.js";
 
 export default (client) => {
     client.handleLogs = async () => {
