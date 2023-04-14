797bd11bc1f479ff38c5e35aa337056382a9aaaa
cassius23 <emilekm@gmail.com>
2023-04-14 20:13:41 +0200

    Format

--- functions/logs/utils.test.js
+++ functions/logs/utils.test.js
@@ -1,73 +1,73 @@
 import { parseAdminCommand, prepDescription } from "./utils.js";
 
 test("prepDescription", () => {
-  const player = {
-    name: "Name",
-  }
+    const player = {
+        name: "Name",
+    };
 
-  const playerWithTag = {
-    tag: "TAG",
-    name: "Name",
-  }
+    const playerWithTag = {
+        tag: "TAG",
+        name: "Name",
+    };
 
-  const tests = [
-    {
-      blueprint: {},
-      data: {
-        body: "Test",
-        issuer: player,
-      },
-      out: "**Performed by: **`Name`\n**Reason : **`Test`"
-    }, {
-      blueprint: {
-        header: null,
-      },
-      data: {
-        body: "Test",
-        issuer: player,
-      },
-      out: "**Performed by: **`Name`\nTest"
-    }, {
-      blueprint: {},
-      data: {
-        body: null,
-        issuer: player,
-      },
-      out: "**Performed by: **`Name`"
-    }, { // receiver
-      blueprint: {},
-      data: {
-        body: null,
-        issuer: playerWithTag,
-        receiver: player,
-      },
-      out: "**Performed by: **`TAG Name`\n**On user: ** Name"
-    }
-  ]
+    const tests = [
+        {
+            blueprint: {},
+            data: {
+                body: "Test",
+                issuer: player,
+            },
+            out: "**Performed by: **`Name`\n**Reason : **`Test`"
+        }, {
+            blueprint: {
+                header: null,
+            },
+            data: {
+                body: "Test",
+                issuer: player,
+            },
+            out: "**Performed by: **`Name`\nTest"
+        }, {
+            blueprint: {},
+            data: {
+                body: null,
+                issuer: player,
+            },
+            out: "**Performed by: **`Name`"
+        }, { // receiver
+            blueprint: {},
+            data: {
+                body: null,
+                issuer: playerWithTag,
+                receiver: player,
+            },
+            out: "**Performed by: **`TAG Name`\n**On user: ** `Name`"
+        }
+    ];
 
-  tests.forEach(t => {
-    expect(prepDescription(t.blueprint, t.data)).toEqual(t.out)
-  })
-})
+    tests.forEach(t => {
+        expect(prepDescription(t.blueprint, t.data)).toEqual(t.out);
+    });
+});
 
 test("parseAdminCommand", () => {
-  const tests = [
-    {
-      in: "[2023-04-01 10:11] !INIT           performed by 'TAG name': ",
-      out: {
-        "body": "",
-        "command": "INIT",
-        "issuer": {
-          "name": "name",
-          "tag": "TAG",
-        },
-        "issuer_type": "user",
-        "orig": "[2023-04-01 10:11] !INIT performed by 'TAG name':",
-      }
-    }
-  ]
+    const tests = [
+        {
+            in: "[2023-04-01 10:11] !INIT           performed by 'TAG name': ",
+            out: {
+                "body": "",
+                "command": "INIT",
+                "issuer": {
+                    "name": "name",
+                    "tag": "TAG",
+                },
+                "issuer_type": "user",
+                "orig": "[2023-04-01 10:11] !INIT performed by 'TAG name':",
+            }
+        }
+    ];
 
-  tests.forEach(t => {
-    expect(parseAdminCommand(t.in)).toEqual(t.out)
-  })
-})
+    tests.forEach(t => {
+        expect(parseAdminCommand(t.in)).toEqual(t.out);
+    });
+});
