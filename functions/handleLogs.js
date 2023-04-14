88f58e438b4be3484a2dde728eecbcbdbce0d1ec
cassius23 <emilekm@gmail.com>
2023-04-14 15:52:55 +0200

    Use the new way of returning

--- functions/handleLogs.js
+++ functions/handleLogs.js
@@ -19,16 +19,14 @@ const watchCommands = (client) => {
     const tailAdmins = new Tail(filenameAdmin, "\n");
 
     tailAdmins.on("line", line => {
-        let adminLogPost = null;
-        let adminLogPostPub = null;
-        adminLogPost, adminLogPostPub = processCommand(line);
+        const embeds = processCommand(line);
 
-        if (adminLogPost !== null) {
-            client.channels.cache.get("995520998554218557").send({ embeds: [adminLogPost] });
+        if ("priv" in embeds) {
+            client.channels.cache.get("995520998554218557").send({ embeds: [embeds.priv] });
         }
 
-        if (adminLogPostPub !== null) {
-            client.channels.cache.get("995387208947204257").send({ embeds: [adminLogPostPub] }); // to change
+        if ("pub" in embeds) {
+            client.channels.cache.get("995387208947204257").send({ embeds: [embeds.pub] }); // to change
         }
     });
 
