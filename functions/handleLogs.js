808ea2d730db33df46bde8afdfe5f018480caffd
cassius23 <emilekm@gmail.com>
2023-04-14 14:14:57 +0200

    Move file handling and sending embeds to main module

--- functions/handleLogs.js
+++ functions/handleLogs.js
@@ -1,6 +1,6 @@
 
 import { watchBanlist } from "./logs/bans.js";
-import { watchCommands } from "./logs/commands.js";
+import { processCommand } from "./logs/commands.js";
 import { watchJoin } from "./logs/join.js";
 
 export default (client) => {
@@ -9,4 +9,30 @@ export default (client) => {
         watchCommands(client);
         watchJoin(client);
     };
+};
+
+const watchCommands = (client) => {
+    const filenameAdmin = "logs/ra_adminlog.txt";
+    if (!fs.existsSync(filenameAdmin)) fs.writeFileSync(filenameAdmin, "");
+    const tailAdmins = new Tail(filenameAdmin, "\n");
+
+    tailAdmins.on("line", line => {
+        let adminLogPost = null;
+        let adminLogPostPub = null;
+        adminLogPost, adminLogPostPub = processCommand(line)
+
+        if (adminLogPost !== null) {
+            client.channels.cache.get("995520998554218557").send({ embeds: [adminLogPost] });
+        }
+
+        if (adminLogPostPub !== null) {
+            client.channels.cache.get("995387208947204257").send({ embeds: [adminLogPostPub] }); // to change
+        }
+    });
+
+    tailAdmins.on("error", function(error) {
+        console.log("ERROR: ", error);
+    });
+
+    tailAdmins.watch();
 };
--- functions/logs/commands.js
+++ functions/logs/commands.js
@@ -3,120 +3,87 @@ import Tail from "always-tail";
 
 import { ISSUERS, adminCommand, parseAdminCommand, reasonedDescription, prepDescription, content } from "./utils.js";
 
-export const watchCommands = (client) => {
-    const filenameAdmin = "logs/ra_adminlog.txt";
-    if (!fs.existsSync(filenameAdmin)) fs.writeFileSync(filenameAdmin, "");
-    const tailAdmins = new Tail(filenameAdmin, "\n");
+export const processCommand = (data) => {
+    const adminLogSplit = data.split(" ");
+    const skip = [
+        "SESSIONERR",
+        "!HASH",
+        "!TEMPBAN",
+        "!BAN",
+        "!MAPVOTE",
+    ];
+    let adminLogPost = null;
+    let adminLogPostPub = null;
 
-    tailAdmins.on("line", process(client));
+    if (!skip.find((s) => adminLogSplit[2].includes(s))) {
 
-    tailAdmins.on("error", function(error) {
-        console.log("ERROR: ", error);
-    });
+        const parsed = parseAdminCommand(data);
 
-    tailAdmins.watch();
-};
+        if (parsed.command === "REPORTP" || parsed.command === "REPORT") {
+            adminLogPost = reportPlayer(parsed);
+        } else if (parsed.command === "KICK") {
+            adminLogPost, adminLogPostPub = kickPlayer(parsed);
+        } else if (parsed.command === "WARN") {
+            adminLogPost = adminCommand(parsed)
+                .setColor(0XEBCD34);
+        } else if (parsed.command === "RESIGN") {
+            adminLogPost = adminCommand(parsed)
+                .setColor(0Xbba170);
+        } else if (parsed.command === "KILL") {
+            adminLogPost = adminCommand(parsed)
+                .setColor(0Xff8bcb);
+        } else if (parsed.command === "INIT") {
+            adminLogPost = adminCommand(parsed)
+                .setColor(0X3f213f)
+                .setDescription(prepDescription(parsed, null, "Adminhashes and -powerlevels have been reloaded"));
+        } else if (parsed.command === "MESSAGE") {
+            const msgContent = content(parsed.body);
 
-const process = (client) => {
-    return (data) => {
-        const adminLogSplit = data.split(" ");
-        const skip = [
-            "SESSIONERR",
-            "!HASH",
-            "!TEMPBAN",
-            "!BAN",
-            "!MAPVOTE",
-        ];
+            adminLogPost = adminCommand(parsed)
+                .setColor(0X2c37ca)
+                .setDescription(prepDescription(parsed, "Message", msgContent));
+        } else if (parsed.command === "SAY") {
+            const sayContent = content(parsed.body);
 
-        if (!skip.find((s) => adminLogSplit[2].includes(s))) {
-            let adminLogPost = {};
-            let adminLogPostPub = {};
+            adminLogPost = adminCommand(parsed)
+                .setColor(0X34EB6B)
+                .setDescription(prepDescription(parsed, "Content", sayContent));
+        } else if (parsed.command === "SETNEXT") {
+            adminLogPost = adminCommand(parsed)
+                .setColor(0X10a17d)
+                .setDescription(prepDescription(parsed, "Map"));
 
-            const parsed = parseAdminCommand(data);
+            adminLogPostPub = adminCommand(parsed)
+                .setColor(0X10a17d)
+                .setDescription(prepDescription(parsed))
+                .setFooter({});
+        } else if (parsed.command === "SWITCH") {
+            adminLogPost = adminCommand(parsed)
+                .setColor(0X3292a8)
+                .setDescription(prepDescription(parsed, "When"));
+        } else if (parsed.command === "RUNNEXT") {
+            fs.writeFile("logs/gungame_winner.txt", "Admin \"!runnext\"", function(err) {
+                if (err) {
+                    // append failed
+                } else {
+                    // done
+                }
+            });
 
-            if (parsed.command === "REPORTP" || parsed.command === "REPORT") {
-                adminLogPost = reportPlayer(parsed);
-                client.channels.cache.get("995520998554218557").send({ embeds: [adminLogPost] });
-            } else if (parsed.command === "KICK") {
-                adminLogPost, adminLogPostPub = kickPlayer(parsed);
-                client.channels.cache.get("995387208947204257").send({ embeds: [adminLogPostPub] }); // to change
-                client.channels.cache.get("995520998554218557").send({ embeds: [adminLogPost] });
-            } else if (parsed.command === "WARN") {
-                adminLogPost = adminCommand(parsed)
-                    .setColor(0XEBCD34);
-                client.channels.cache.get("995520998554218557").send({ embeds: [adminLogPost] });
-            } else if (parsed.command === "RESIGN") {
-                adminLogPost = adminCommand(parsed)
-                    .setColor(0Xbba170);
-                client.channels.cache.get("995520998554218557").send({ embeds: [adminLogPost] });
-            } else if (parsed.command === "KILL") {
-                adminLogPost = adminCommand(parsed)
-                    .setColor(0Xff8bcb);
-                client.channels.cache.get("995520998554218557").send({ embeds: [adminLogPost] });
-            } else if (parsed.command === "INIT") {
-                adminLogPost = adminCommand(parsed)
-                    .setColor(0X3f213f)
-                    .setDescription(prepDescription(parsed, null, "Adminhashes and -powerlevels have been reloaded"));
+            adminLogPost = adminCommand(parsed)
+                .setColor(0X085441)
+                .setDescription(prepDescription(parsed, null, " "));
 
-                client.channels.cache.get("995520998554218557").send({ embeds: [adminLogPost] });
-            } else if (parsed.command === "MESSAGE") {
-                const msgContent = content(parsed.body);
-
-                adminLogPost = adminCommand(parsed)
-                    .setColor(0X2c37ca)
-                    .setDescription(prepDescription(parsed, "Message", msgContent));
-
-                client.channels.cache.get("995520998554218557").send({ embeds: [adminLogPost] });
-            } else if (parsed.command === "SAY") {
-                const sayContent = content(parsed.body);
-
-                adminLogPost = adminCommand(parsed)
-                    .setColor(0X34EB6B)
-                    .setDescription(prepDescription(parsed, "Content", sayContent));
-
-                client.channels.cache.get("995520998554218557").send({ embeds: [adminLogPost] });
-            } else if (parsed.command === "SETNEXT") {
-                adminLogPost = adminCommand(parsed)
-                    .setColor(0X10a17d)
-                    .setDescription(prepDescription(parsed, "Map"));
-
-                adminLogPostPub = adminCommand(parsed)
-                    .setColor(0X10a17d)
-                    .setDescription(prepDescription(parsed))
-                    .setFooter({});
-
-                client.channels.cache.get("995520998554218557").send({ embeds: [adminLogPost] });
-                client.channels.cache.get("995387208947204257").send({ embeds: [adminLogPostPub] });
-            } else if (parsed.command === "SWITCH") {
-                adminLogPost = adminCommand(parsed)
-                    .setColor(0X3292a8)
-                    .setDescription(prepDescription(parsed, "When"));
-
-                client.channels.cache.get("995520998554218557").send({ embeds: [adminLogPost] });
-            } else if (parsed.command === "RUNNEXT") {
-                fs.writeFile("logs/gungame_winner.txt", "Admin \"!runnext\"", function(err) {
-                    if (err) {
-                        // append failed
-                    } else {
-                        // done
-                    }
-                });
-
-                adminLogPost = adminCommand(parsed)
-                    .setColor(0X085441)
-                    .setDescription(prepDescription(parsed, null, " "));
-
-                client.channels.cache.get("995520998554218557").send({ embeds: [adminLogPost] });
-            } else if (parsed.command === "MAPVOTERESULT") {
-                adminLogPost, adminLogPostPub = mapvoteResult(parsed);
-
-                client.channels.cache.get("995387208947204257").send({ embeds: [adminLogPostPub] });
-                client.channels.cache.get("995520998554218557").send({ embeds: [adminLogPost] });
-            } else {
-                client.channels.cache.get("995520998554218557").send("`" + adminLogSplit + "`");
-            }
+        } else if (parsed.command === "MAPVOTERESULT") {
+            adminLogPost, adminLogPostPub = mapvoteResult(parsed);
         }
-    };
+        // TODO: figure out a way to handle undefined
+        // else {
+        //     client.channels.cache.get("995520998554218557").send("`" + adminLogSplit + "`");
+        // }
+    }
+
+    return [adminLogPost, adminLogPostPub]
 };
 
 const reportPlayer = (data) => {
