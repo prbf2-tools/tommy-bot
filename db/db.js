b1b4d57319c9dc51e1a21a2e8575b9e436b57dc4
cassius23 <emilekm@gmail.com>
2023-04-27 00:33:44 +0200

    Cleanup

--- db/db.js
+++
@@ -1,8 +0,0 @@
-import low from "lowdb";
-import FileSync from "lowdb/adapters/FileSync.js";
-
-const adapter = new FileSync("./db/db.json");
-const db = low(adapter);
-db.defaults({ hashDb: [] }).write();
-
-export default db;
--- events/ready.js
+++
@@ -1,8 +0,0 @@
-
-export default {
-    name: "ready",
-    once: true,
-    async execute() {
-        console.log("Ready!");
-    }
-};
