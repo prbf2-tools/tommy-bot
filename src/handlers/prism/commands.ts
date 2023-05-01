c21be2960a2d747bf1c502c5b2a6b5b60bb720b7
cassius23 <emilekm@gmail.com>
2023-05-01 16:15:56 +0200

    Add commands queue to return response

--- src/handlers/prism/commands.ts
+++ src/handlers/prism/commands.ts
@@ -1,17 +1,62 @@
-import prism from "./index.js";
+import prism, { PRISM, Subject } from "./index.js";
 
-export function init() {
-    prism.sendChat("!init");
+class CommandsQueue {
+    private point: Promise<void> | null = null;
+    private prism: PRISM = prism
+
+    async add(command: string): Promise<string> {
+        const p = new Promise<string>(resolve => {
+
+            const point = this.point;
+
+            const handle = async () => {
+                const chatListener = (fields: string[][]) => {
+                    // TODO: find response logic
+                    const response = ""
+
+                    this.prism.removeListener(Subject.Chat, chatListener)
+
+                    resolve(response)
+                }
+
+                this.prism.on(Subject.Chat, chatListener)
+
+                this.prism.sendChat(command)
+            }
+
+            if (point !== null) {
+                point.finally(() => {
+                    handle()
+                })
+            } else {
+                handle()
+            }
+        })
+
+        this.point = new Promise<void>(resolve => {
+            p.finally(() => {
+                resolve()
+            })
+        })
+
+        return p
+    }
 }
 
-export function unbanid(hashID: string) {
-    prism.sendChat(`!unbanid ${hashID}`);
+const queue = new CommandsQueue()
+
+export function init(): Promise<string> {
+    return queue.add("!init");
 }
 
-export function banid(hashID: string, reason: string) {
-    prism.sendChat(`!banid ${hashID} ${reason}`);
+export function unbanid(hashID: string): Promise<string> {
+    return queue.add(`!unbanid ${hashID}`);
 }
 
-export function timebanid(hashID: string, duration: string, reason: string) {
-    prism.sendChat(`!timebanid ${hashID} ${duration} ${reason}`);
+export function banid(hashID: string, reason: string): Promise<string> {
+    return queue.add(`!banid ${hashID} ${reason}`);
+}
+
+export function timebanid(hashID: string, duration: string, reason: string): Promise<string> {
+    return queue.add(`!timebanid ${hashID} ${duration} ${reason}`);
 }
